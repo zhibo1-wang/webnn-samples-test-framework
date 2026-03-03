957808b3a3646a1e0111787c53ac0772465ba6eb
Zhibo Wang <zhibo1.wang@intel.com>
2026-03-03 15:04:33 +0800

    Refactor test cases PART III: developer preview

--- src/cases/developer-preview/segment-anything.js
+++ src/cases/developer-preview/segment-anything.js
@@ -1,100 +1,60 @@
 const util = require("../../utils/util.js");
+const BaseSample = require("./base-sample.js");
 const pageElementTotal = require("../../page-elements/developer-preview.js");
-const _ = require("lodash");
 
-async function segmentAnythingTest({ config, backend, dataType, model } = {}) {
-  const source = "developer-preview";
-  const sample = "segment-anything";
-  const pageElement = pageElementTotal[sample];
-
-  const testExecution = async (backend, dataType, model = "") => {
-    let result = {};
-    console.log(`${source} ${sample} ${backend} ${dataType} ${model} testing...`);
-
-    const screenshotFilename = model
-      ? `${source}_${sample}_${backend}_${dataType}_${model}`
-      : `${source}_${sample}_${backend}_${dataType}`;
-    let browser;
-    let page;
-
-    try {
-      browser = await util.launchBrowser(config);
-      page = (await browser.pages())[0];
-      page.setDefaultTimeout(config["timeout"]);
-
-      const urlArguments = config[source][sample]["urlArgs"][backend];
-      await page.goto(`${config["developerPreviewBasicUrl"]}${config["developerPreviewUrl"][sample]}${urlArguments}`, {
-        waitUntil: "networkidle0"
-      });
-
-      // wait model load complete
-      await Promise.race([
-        page.waitForSelector(pageElement["imgCanvas"], { visible: true }),
-        util.throwOnUncaughtException(page)
-      ]);
-
-      // get canvas image location
-      const imageRect = await page.evaluate((pageElement) => {
-        const imageElement = document.querySelector(pageElement["imgCanvas"]);
-        const imageObj = imageElement.getBoundingClientRect();
-        return JSON.parse(JSON.stringify(imageObj));
-      }, pageElement);
-
-      // move the mouse to a random spot of canvas image
-      let spotX = config[source][sample]["imageSpot"]["x"];
-      let spotY = config[source][sample]["imageSpot"]["y"];
-      let x = Math.floor(spotX * imageRect.width) + imageRect.left;
-      let y = Math.floor(spotY * imageRect.height) + imageRect.top;
-      // click the spot of canvas image
-      await page.mouse.click(x, y);
-      // wait results appear
-      await page.waitForSelector(pageElement.decoderLatency, { visible: true });
-      await page.waitForFunction(
-        async (pageElement) => document.querySelector(pageElement.decoderLatency)?.textContent,
-        {},
-        pageElement
-      );
-      const log = await page.$eval(pageElement.logPanel, (el) => el.textContent);
-      result = {
-        encoder: {
-          buildTime: log.match(/SAM ViT-B Encoder \(FP(?:16|32)\) create time: (\d+\.?\d*)ms/)[1],
-          inferenceTime: log.match(/Encoder execution time: (\d+\.?\d*)ms/)[1]
-        },
-        decoder: {
-          buildTime: log.match(/SAM ViT-B Decoder \(FP(?:16|32)\) create time: (\d+\.?\d*)ms/)[1],
-          inferenceTime: await page.$eval(pageElement.decoderLatency, (el) => el.textContent)
-        }
-      };
-      console.log("Test Results: ", result);
-    } catch (error) {
-      result = { encoder: { error: error.message }, decoder: { error: error.message } };
-      console.warn(error);
-    } finally {
-      if (page) await util.saveScreenshot(page, screenshotFilename);
-      if (browser) await browser.close();
-    }
-    if (model) {
-      return { [model]: result[model] };
-    } else {
-      return result;
-    }
-  };
-
-  let results = {};
-  if (backend && dataType) {
-    _.set(results, [sample, backend, dataType], await testExecution(backend, dataType, model));
-  } else {
-    for (let _backend in config[source][sample]) {
-      if (!["cpu", "gpu", "npu"].includes(_backend)) {
-        continue;
-      }
-      for (let _dataType in config[source][sample][_backend]) {
-        _.set(results, [sample, _backend, _dataType], await testExecution(_backend, _dataType));
-      }
-    }
+class SegmentAnything extends BaseSample {
+  constructor(config) {
+    super(config, "segment-anything");
   }
 
-  return results;
+  async run(page, backend, dataType, model) {
+    const pageElement = pageElementTotal[this.sample];
+
+    // wait model load complete
+    await Promise.race([
+      page.waitForSelector(pageElement["imgCanvas"], { visible: true }),
+      util.throwOnUncaughtException(page)
+    ]);
+
+    // get canvas image location
+    const imageRect = await page.evaluate((pageElement) => {
+      const imageElement = document.querySelector(pageElement["imgCanvas"]);
+      const imageObj = imageElement.getBoundingClientRect();
+      return JSON.parse(JSON.stringify(imageObj));
+    }, pageElement);
+
+    // move the mouse to a random spot of canvas image
+    let spotX = this.config[this.source][this.sample]["imageSpot"]["x"];
+    let spotY = this.config[this.source][this.sample]["imageSpot"]["y"];
+    let x = Math.floor(spotX * imageRect.width) + imageRect.left;
+    let y = Math.floor(spotY * imageRect.height) + imageRect.top;
+
+    // click the spot of canvas image
+    await page.mouse.click(x, y);
+    // wait results appear
+    await page.waitForSelector(pageElement.decoderLatency, { visible: true });
+    await page.waitForFunction(
+      async (pageElement) => document.querySelector(pageElement.decoderLatency)?.textContent,
+      {},
+      pageElement
+    );
+    const log = await page.$eval(pageElement.logPanel, (el) => el.textContent);
+    const result = {
+      encoder: {
+        buildTime: log.match(/SAM ViT-B Encoder \(FP(?:16|32)\) create time: (\d+\.?\d*)ms/)[1],
+        inferenceTime: log.match(/Encoder execution time: (\d+\.?\d*)ms/)[1]
+      },
+      decoder: {
+        buildTime: log.match(/SAM ViT-B Decoder \(FP(?:16|32)\) create time: (\d+\.?\d*)ms/)[1],
+        inferenceTime: await page.$eval(pageElement.decoderLatency, (el) => el.textContent)
+      }
+    };
+    console.log("Test Results: ", result);
+    return result;
+  }
 }
 
-module.exports = segmentAnythingTest;
+module.exports = async function ({ config, backend, dataType, model }) {
+  const instance = new SegmentAnything(config);
+  return await instance.execute(backend, dataType, model);
+};
--- src/cases/developer-preview/text-generation.js
+++ src/cases/developer-preview/text-generation.js
@@ -1,79 +1,55 @@
-const _ = require("lodash");
+const util = require("../../utils/util.js");
+const BaseSample = require("./base-sample.js");
 
-const util = require("../../utils/util");
-
-module.exports = async function ({ config, backend, dataType, model }) {
-  const args = [];
-  if (backend && dataType && model) {
-    args.push([backend, dataType, model]);
-  } else {
-    for (const [backend, dataTypes] of Object.entries(config["developer-preview"]["text-generation"])) {
-      if (backend in ["cpu", "gpu", "npu"]) {
-        for (const [dataType, models] of Object.entries(dataTypes)) {
-          for (const model of models) {
-            args.push([backend, dataType, model]);
-          }
-        }
-      }
-    }
+class TextGeneration extends BaseSample {
+  constructor(config) {
+    super(config, "text-generation");
+    this.config.timeout *= 3;
   }
-  config.timeout *= 3;
-  const results = {};
-  for (const [backend, dataType, model] of args) {
-    const browser = await util.launchBrowser(config);
-    const page = (await browser.pages())[0];
-    page.setDefaultTimeout(config.timeout);
+
+  async run(page, backend, dataType, model) {
     const result = {
       ttft: [],
       tps: []
     };
-    try {
-      const urlArgs = new URLSearchParams();
-      urlArgs.append("devicetype", backend);
-      urlArgs.append("model", model);
-      await page.goto(config.developerPreviewBasicUrl + config.developerPreviewUrl["text-generation"] + "?" + urlArgs, {
-        waitUntil: "networkidle0"
-      });
-      await Promise.race([
-        (async function () {
+
+    await Promise.race([
+      (async () => {
+        await util.waitForElementEnabled(page, "#send-button");
+        for (const { question, answer } of this.sampleConfig.cases) {
+          await page.type("#user-input", "This is a correctness test. " + question);
+          await page.click("#send-button");
           await util.waitForElementEnabled(page, "#send-button");
-          for (const { question, answer } of config["developer-preview"]["text-generation"].cases) {
-            await page.type("#user-input", "This is a correctness test. " + question);
-            await page.click("#send-button");
-            await util.waitForElementEnabled(page, "#send-button");
 
-            const timeToFirstToken = await page.$eval(
-              '.performance-data[title="Time to first token"] .tokens-per-second-performance-data',
-              (el) => el.textContent.trim()
-            );
-            const tokensPerSecond = await page.$eval(
-              '.performance-data[title="tokens per second"] .tokens-per-second-performance-data',
-              (el) => el.textContent.trim()
-            );
-            result.ttft.push(util.formatTimeResult(timeToFirstToken));
-            result.tps.push(util.formatTimeResult(tokensPerSecond));
+          const timeToFirstToken = await page.$eval(
+            '.performance-data[title="Time to first token"] .tokens-per-second-performance-data',
+            (el) => el.textContent.trim()
+          );
+          const tokensPerSecond = await page.$eval(
+            '.performance-data[title="tokens per second"] .tokens-per-second-performance-data',
+            (el) => el.textContent.trim()
+          );
+          result.ttft.push(util.formatTimeResult(timeToFirstToken));
+          result.tps.push(util.formatTimeResult(tokensPerSecond));
 
-            const lastResponse = await page.$$(".response-message");
-            const responseText = await (
-              await lastResponse[lastResponse.length - 1].getProperty("textContent")
-            ).jsonValue();
-            if (responseText.trim() !== answer.trim()) {
-              throw new Error(
-                `Got "${responseText.trim()}", expected "${answer.trim()}" for the question "${question}"`
-              );
-            }
+          const lastResponse = await page.$$(".response-message");
+          const responseText = await (
+            await lastResponse[lastResponse.length - 1].getProperty("textContent")
+          ).jsonValue();
+          if (responseText.trim() !== answer.trim()) {
+            throw new Error(`Got "${responseText.trim()}", expected "${answer.trim()}" for the question "${question}"`);
           }
-        })(),
-        util.throwOnUncaughtException(page)
-      ]);
-    } catch (error) {
-      console.warn(error.message);
-      result.error = error.message.substring(0, config.errorMsgMaxLength);
-    } finally {
-      await util.saveScreenshot(page, `text-generation_${backend}_${dataType}_${model}`);
-      await browser.close();
-    }
-    _.set(results, [backend, dataType, model], result);
+        }
+      })(),
+      util.throwOnUncaughtException(page)
+    ]);
+
+    console.log(result);
+    return result;
   }
-  return { "text-generation": results };
+}
+
+module.exports = async function ({ config, backend, dataType, model }) {
+  const textGeneration = new TextGeneration(config);
+  return await textGeneration.execute(backend, dataType, model);
 };
